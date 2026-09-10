e51be0a7df3fa4e3664d9701829bf88a76e6ded7
Yves Esteves <yves.yan@hotmail.com>
2026-09-10 13:59:31 -0300

    fix: Habilita coreLibraryDesugaring no arquivo KTS

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,9 +10,9 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
-        coreLibraryDesugaringEnabled true 
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true 
     }
 
     defaultConfig {
@@ -47,4 +47,9 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
